4ab24a21ae9952851883fc9113eecd6c55a1ff70
Aaditya <aadityaveerbhan@gmail.com>
2024-08-30 20:22:07 +0530

    TASK-1 CALCULATOR

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var inputBox = document.getElementById('inputBox');
+
+    var buttons = document.querySelectorAll('button');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var buttonText = button.innerText;
+
+            switch (buttonText) {
+                case 'AC':
+                    inputBox.value = '';
+                    break;
+                case 'DEL':
+                    inputBox.value = inputBox.value.slice(0, -1);
+                    break;
+                case '=':
+                    try {
+                        inputBox.value = eval(inputBox.value);
+                    } catch (error) {
+                        inputBox.value = 'Error';
+                    }
+                    break;
+                default:
+                    inputBox.value += buttonText;
+            }
+        });
+    });
+});
